4bda1bd09ff0bae2b5986286d1b35d870567be4d
Henry Riddall <Henry.Riddall@softwire.com>
2024-04-05 14:57:35 +0100

    Add jump animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./todo_app/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        jump: {
+          "0%, 100%": {
+            transform: "translateY(0)",
+          },
+          "33%": {
+            transform: "translateY(20px)",
+          },
+          "33%": {
+            transform: "translateY(-20px)",
+          },
+        },
+      },
+      animation: {
+        jump: "jump 0.7s ease normal",
+      },
+    },
   },
   plugins: [],
-}
-
+};
