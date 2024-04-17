3ad8ab1f0a4b1ed2a1668b779e39ec37ca8fec16
Henry Riddall <Henry.Riddall@softwire.com>
2024-04-17 19:26:04 +0100

    Review markups

--- todo_app/tests/integration_tests/test_index_page.py
+++ todo_app/tests/integration_tests/test_index_page.py
@@ -5,9 +5,6 @@ from todo_app.tests.conftest import stub
 
 
 def test_index_page(monkeypatch, client):
-    # Replace requests.get(url) with our own function
-    monkeypatch.setattr(requests, "get", stub)
-
     # Make a request to our app's index page
     response = client.get("/")
 
