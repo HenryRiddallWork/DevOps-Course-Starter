952a54ed4da724085262802778603afa366d9fa7
Henry Riddall <Henry.Riddall@softwire.com>
2024-10-22 15:41:02 +0100

    remove unused files/code

--- todo_app/data/services/trello_items_service.py
+++
@@ -1,7 +0,0 @@
-from todo_app.data.repositories.trello_repository import TrelloRepository
-from todo_app.data.services.base_items_service import BaseItemsService
-
-
-class TrelloItemsService(BaseItemsService):
-    def __init__(self):
-        self.repository = TrelloRepository()
