ae0e3bfa85e0d794dcdc3927545adb21c33cedc7
Henry Riddall <Henry.Riddall@softwire.com>
2024-10-29 16:48:51 +0000

    Add OAuth login

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,6 +4,7 @@ from todo_app.data.models.item import Item, Status
 from todo_app.data.services.mongo_items_service import MongoItemsService
 from todo_app.flask_config import Config
 from todo_app.models.index_model import IndexModel
+from todo_app.oauth import blueprint, login_required
 
 
 def create_app():
@@ -11,7 +12,10 @@ def create_app():
     app.config.from_object(Config())
     mongoItemsService = MongoItemsService()
 
+    app.register_blueprint(blueprint, url_prefix="/login")
+
     @app.route("/")
+    @login_required
     def index():
         items = mongoItemsService.get_items()
         sorted_items = sorted(items, key=lambda item: item.id)
@@ -19,16 +23,19 @@ def create_app():
         return render_template("/index.html", view_model=item_view_model)
 
     @app.route("/", methods=["POST"])
+    @login_required
     def add_todo_item():
         mongoItemsService.add_item(request.form.get("title"))
         return redirect("/")
 
     @app.route("/delete", methods=["POST"])
+    @login_required
     def remove_todo_item():
         mongoItemsService.remove_item_by_id(request.form.get("item_id"))
         return redirect("/")
 
     @app.route("/toggle", methods=["POST"])
+    @login_required
     def toggle_todo_item_status():
         current_item = Item(
             request.form.get("item_id"),
